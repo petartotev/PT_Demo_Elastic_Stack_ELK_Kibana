b635d8c786e6f618476360ca389b0ecf5bcfad97
petartotev <petar@petartotev.net>
2024-06-15 09:45:24 +0300

    PetarTotev.Demo.ElasticSearchKibana solution added

---
+++ src/PetarTotev.Demo.ElasticSearchKibana.EndPoint/Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace PetarTotev.Demo.ElasticSearchKibana.EndPoint.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ src/PetarTotev.Demo.ElasticSearchKibana.EndPoint/Program.cs
@@ -0,0 +1,51 @@
+
+using Serilog.Sinks.Elasticsearch;
+using Serilog;
+
+namespace PetarTotev.Demo.ElasticSearchKibana.EndPoint
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Configure Serilog
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+                {
+                    AutoRegisterTemplate = true,
+                })
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
